a2c6a9ce2a4d67fc6b8b881adabceee5692bb837
ahmadzaki8828 <ahmadzaki8828@gmail.com>
2024-05-23 01:48:28 -0400

    stripe checkout api

---
+++ app/api/stripe/route.ts
@@ -0,0 +1,63 @@
+import { NextResponse } from "next/server";
+import prismadb from "@/lib/prismadb";
+import { stripe } from "@/lib/stripe";
+import { absolteUrl } from "@/lib/utils";
+import { url } from "inspector";
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+const settingsUrl = absolteUrl("/settings");
+
+export async function GET() {
+  try {
+    const { userId } = auth();
+    const user = await currentUser();
+
+    if (!userId || !user) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const userSubscription = await prismadb.userSubsciption.findUnique({
+      where: { userId: userId },
+    });
+
+    if (userSubscription && userSubscription.stripeCustomerId) {
+      const stripeSession = await stripe.billingPortal.sessions.create({
+        customer: userSubscription.stripeCustomerId,
+        return_url: settingsUrl,
+      });
+
+      return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+    }
+
+    const stripeSession = await stripe.checkout.sessions.create({
+      success_url: settingsUrl,
+      cancel_url: settingsUrl,
+      payment_method_types: ["card"],
+      mode: "subscription",
+      billing_address_collection: "auto",
+      customer_email: user.emailAddresses[0].emailAddress,
+      line_items: [
+        {
+          price_data: {
+            currency: "USD",
+            product_data: {
+              name: "Athena Pro",
+              description: "Unlimited AI Uses",
+            },
+            unit_amount: 2000,
+            recurring: { interval: "month" },
+          },
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        userId,
+      },
+    });
+
+    return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+  } catch (error) {
+    console.log("STRIPE_ERROR", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
